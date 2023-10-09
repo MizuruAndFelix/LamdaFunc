e1c16da946e74f9d8fc0654a6e7203cceb36dffd
Misuiro <113217149+MizuruAndFelix@users.noreply.github.com>
2023-10-09 18:12:06 +0300

    1

---
+++ LamdaFunction/LamdaFunction.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> Vector = {4, 7, 9, 14, 12, 15}; // вторые значения vector<int> Vector = {4 , 21 , 27, 14 , 12 , 45};                      
+    auto print = [](const int& n) { std::cout << n << '\t'; }; // 
+
+
+    cout << "Input:\t";
+    for_each(Vector.cbegin(), Vector.cend(), print);
+    cout << '\n';
+
+    for_each(Vector.begin(), Vector.end(), [&Vector](int& i)
+        {
+            if (i % 2 != 0)
+            {
+                (i) *= 3;
+            }
+        });
+
+    cout << "Output:\t";
+    for_each(Vector.cbegin(), Vector.cend(), print);
+
+
+    cout << "\n\nEND!\n";
+}
